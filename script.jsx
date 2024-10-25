d5d6d889854a4bc2031be7c14b9fd23b52d13c98
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-10-25 17:16:37 -0400

    finished but will do the css later

--- script.jsx
+++ script.jsx
@@ -22,22 +22,41 @@ function App() {
        const key = e.key.toUpperCase();
        if(beat.hasOwnProperty(key)){
            play(key);
+           NameDisplay(key);
        }
    };
    const handleClick = key => {
        play(key);
+       NameDisplay(key);
+   }
+   const NameDisplay = (key) => {
+       const title = document.getElementById("title");
+       const names = {
+        Q:'Heater 1',
+        W:'Heater 2',
+        E:'Heater 3',
+        A:'Heater 4',
+        S:'Clap',
+        D:'Open HH',
+        Z:'Kick n Hat',
+        X:'Kick',
+        C:'Closed HH'
+       }
+       title.innerHTML = names[key] || "";
    }
     return (
-    <div id="drum-machine" onKeyDown={handleKeydown} tableIndex="0">
+    <div id="drum-machine" onKeyDown={handleKeydown} tabIndex="0">
         <div id="display">
+        <h1 id="title" ></h1>
+        <div class="drum-pads">
           {Object.keys(beat).map(key => (
             <button className="drum-pad" id={key} onClick={() => handleClick(key)} key={key}>
                  {key}
             <audio className="clip" id={key} src={beat[key]} preload="auto"></audio>
             </button>
-          ))}
+          ))} 
+          </div>
         </div>
-        <
         </div>
 
     );
